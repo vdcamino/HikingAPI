0798b756d52ea170b3a7763c7dde7fd159e59db8
Victor Da Camino <victordacamino@gmail.com>
2022-06-10 23:12:46 +0200

    Add project files.

---
+++ Models/Issue.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HikingAPI.Models
+{
+    public class Issue
+    {
+        public int ID { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Description { get; set; }
+        public Priority Priority { get; set; }
+        public IssueType Type { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime? Completed { get; set; }
+    }
+        
+    public enum Priority
+    {
+        Low, Medium, High
+    }
+
+    public enum IssueType
+    {
+        Feature, Bug, Documentation
+    }
+}
